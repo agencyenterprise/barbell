777a6377e700b2efc83590bb6ef162681c091a11
Fred Murakawa <fredbmurakawa@gmail.com>
2022-11-03 15:41:16 -0700

    proj

---
+++ Barbell/Models/HNStory.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+struct HNStory: Decodable, FeedItemProtocol {
+    let id: String
+    let author = "hn"
+    let title: String
+    var url: String { "https://news.ycombinator.com/item?id=\(id)" }
+    let source = Source.hackerNews
+    
+    enum CodingKeys: CodingKey {
+        case author, id, title, url
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        title = try container.decode(String.self, forKey: .title).unescape
+        let intID = try container.decode(Int.self, forKey: .id)
+        id = "\(intID)"
+    }
+}
